76b93ea0a7a05b26d2fbb63fb85db1e948972283
Swathikala2004 <swathikala65@gmail.com>
2026-06-06 23:02:45 +0530

    Added axios dependency

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,3 +5,10 @@ const api = axios.create({
 });
 
 export default api;
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default api;
